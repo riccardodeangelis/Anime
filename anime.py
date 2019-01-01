05c0329f0c99131db2bd1badf9321f487cc2c73b
Riccardo <>
2019-01-01 19:10:59 +0100

    first commit

---
+++ anime.py
@@ -0,0 +1,28 @@
+print("Ciao! Basta che tu mi scriva una o due parole all' inizio della sigla di un anime ,che io lo indovino! Proviamo? ;")
+
+a = input("Per favore scrivi due parole appartenenti ad una sigla!: ")
+
+
+
+if a == ("oshiete"):
+
+    print("Troppo semplice! È Tokyo Ghoul")
+
+
+
+if a == ("shizukesa ga"):
+
+    print("Beh difficile questa.... ah sì!My Hero Academia")
+
+
+
+if a == ("mittonetete"):
+
+    print("Vabbè, cerca di chiedere cose più semplici!! Sword Art Online")
+
+
+
+if a == ("shizuked"):
+
+    print("Full Metal Alchemist")
+
